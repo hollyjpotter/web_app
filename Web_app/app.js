95375ce357cad1fd8223ab863beeb52b3a12ae33
hollyjpotter <41168067+hollyjpotter@users.noreply.github.com>
2023-08-13 14:08:45 -0700

    Add files via upload

--- Web_app/app.js
+++ Web_app/app.js
@@ -5,26 +5,27 @@ const config = require('./modules/config');
 const port = config.server.port;
 const host = config.server.host;
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/public/pages');
-
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
 // Define a route for the root URL ("/")
 app.get('/', (req, res) => {
-    // Render the index view (assuming your index.html is converted to index.hbs)
-    res.render('index');
+    res.render('index', {
+        angularJSCDN: config.angularJSCDN, // Pass the AngularJS CDN URL to the template
+    });
+    // Serve the main HTML file for your AngularJS app
+    res.sendFile(__dirname + '/public/pages/index.html');
 });
-
-// Set up other routes and middleware as needed...
-// For example, you can add routes like this:
-/*app.get('/about', (req, res) => {
-    // Render the about view (assuming you have an "about.hbs" file)
-    res.render('about');
-});*/
 
 // Start the server
 app.listen(port, host, () => {
     console.log(`Server is running at http://${host}:${port}`);
+});
+
+// Define the AngularJS module and controller
+const angularApp = angular.module('myApp', []);
+
+angularApp.controller('MainController', function($scope) {
+    $scope.appName = 'My App';
+    $scope.currentDate = new Date().toLocaleDateString();
 });
--- Web_app/modules/config.js
+++ Web_app/modules/config.js
@@ -9,4 +9,5 @@ module.exports = {
         uri: 'mongodb+srv://hjpotter:fullstack@fullstackwebdev.z924eyk.mongodb.net/USF_CS386?retryWrites=true&w=majority', // Example MongoDB Atlas URI
     },
     applicationName: 'Endangered Animals App',
+    angularJSCDN: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.8.3/angular.min.js', // Add the AngularJS CDN URL here
 };
