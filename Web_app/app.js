6f909965b98d8469205992d52ab89cd185210806
hollyjpotter <41168067+hollyjpotter@users.noreply.github.com>
2023-08-13 17:38:30 -0700

    Add files via upload

--- Web_app/app.js
+++ Web_app/app.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const app = express();
 const config = require('./modules/config');
-const handlebars = require('express-handlebars');
 
 const port = config.server.port;
 const host = config.server.host;
-
-// Use Handlebars as the template engine
-app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.set('views', __dirname + '/views');
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
 // Define a route for the root URL ("/")
 app.get('/', (req, res) => {
-    // Serve the main HTML file located in ./public/pages/
-    res.sendFile('index.html', { root: __dirname + '/public/pages' });
+    // Serve the index.html file located in the "public/pages" directory
+    res.sendFile(__dirname + '/public/pages/index.html');
 });
 
 // Start the server
--- Web_app/public/scripts/angular.js
+++ Web_app/public/scripts/angular.js
@@ -1,32 +1,20 @@
 // Create the AngularJS module and inject 'ngRoute' as a dependency
-let app = angular.module('spa', ['ngRoute']);
+let app = angular.module('myApp', ['ngRoute']);
 
-// Create constants for the HTML templates
-const htmlHome = '<h2>Home</h2><p>This is the <page>Home</page> Page where you can add your home page content.</p>';
-const htmlAbout = '<h2>About</h2><p>This is the About Page where you can add information about your company.</p>';
-const htmlServices = '<h2>Services</h2><p>This is the Services Page where you can list your services.</p>';
-const htmlProjects = '<h2>Projects</h2><p>This is the Projects Page where you can showcase your projects.</p>';
+angular.module('myApp', [])
+    .controller('AppController', function($scope) {
+        // Set the applicationName from the JavaScript variable
+        $scope.applicationName = applicationName;
+
+        // Set the currentDate in the scope
+        $scope.currentDate = new Date().toLocaleDateString();
+    });
 
 // Configure the routing
-app.config(function($routeProvider, $locationProvider) {
+app.config(function ($routeProvider, $locationProvider) {
     // Set the hash prefix to an empty string (required for compatibility with some servers)
     $locationProvider.hashPrefix('');
 
     // Setup the $routeProvider using .when() for each route
-    $routeProvider
-        .when('/', {
-            template: htmlHome
-        })
-        .when('/about', {
-            template: htmlAbout
-        })
-        .when('/services', {
-            template: htmlServices
-        })
-        .when('/projects', {
-            template: htmlProjects
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
+    // Add your route configurations here
 });
