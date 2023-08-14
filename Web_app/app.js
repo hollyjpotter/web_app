306f87728f5367647a5b54d7c1fcfa0c931f8fb7
hollyjpotter <41168067+hollyjpotter@users.noreply.github.com>
2023-08-13 18:08:26 -0700

    Add files via upload

--- Web_app/app.js
+++ Web_app/app.js
@@ -8,6 +8,12 @@ const host = config.server.host;
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
+// Define an API route to fetch the configuration data
+app.get('/config', (req, res) => {
+    // Send the application name from the config file as a response
+    res.json({ applicationName: config.applicationName });
+});
+
 // Define a route for the root URL ("/")
 app.get('/', (req, res) => {
     // Serve the index.html file located in the "public/pages" directory
--- Web_app/public/scripts/angular.js
+++ Web_app/public/scripts/angular.js
@@ -1,20 +1,25 @@
+// public/scripts/angular.js
 // Create the AngularJS module and inject 'ngRoute' as a dependency
-let app = angular.module('myApp', ['ngRoute']);
 
-angular.module('myApp', [])
-    .controller('AppController', function($scope) {
-        // Set the applicationName from the JavaScript variable
-        $scope.applicationName = applicationName;
+var app = angular.module('myApp', []);
 
-        // Set the currentDate in the scope
-        $scope.currentDate = new Date().toLocaleDateString();
+// Define the AngularJS controller
+app.controller('MainController', function($scope, $http) {
+    // Make an API request to fetch the application name
+    $http.get('/config').then(function(response) {
+        // Set the application name from the API response
+        $scope.applicationName = response.data.applicationName;
+        console.log(applicationName);
     });
-
-// Configure the routing
-app.config(function ($routeProvider, $locationProvider) {
-    // Set the hash prefix to an empty string (required for compatibility with some servers)
-    $locationProvider.hashPrefix('');
-
-    // Setup the $routeProvider using .when() for each route
-    // Add your route configurations here
+    // Set the current date in the scope
+    $scope.currentDate = new Date().toLocaleDateString();
 });
+
+// // Configure the routing
+// app.config(function ($routeProvider, $locationProvider) {
+//     // Set the hash prefix to an empty string (required for compatibility with some servers)
+//     $locationProvider.hashPrefix('');
+//
+//     // Setup the $routeProvider using .when() for each route
+//     // Add your route configurations here
+// });
